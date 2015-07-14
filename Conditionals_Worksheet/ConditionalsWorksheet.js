77d11fa0b43f64bfa46e9b576ad5153140eaa540
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-07-14 13:15:52 -0400

    declared variables
    declared variables for group 1 problem found percent of gas left

--- Conditionals_Worksheet/ConditionalsWorksheet.js
+++ Conditionals_Worksheet/ConditionalsWorksheet.js
@@ -9,3 +9,14 @@ Conditionals Worksheet
  */
 
 //alert("Testing");
+
+
+//Group 1 questions
+//Setup given variables
+var CarMPG = 15;
+var GasTankCapacity = 22;
+var GaugeReading = 50;
+var GaugeReadingPercent = GaugeReading / 100;
+var RemainingGas = GaugeReading * GasTankCapacity;
+
+//Setup conditional with output

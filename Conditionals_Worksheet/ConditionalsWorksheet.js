1aff6ef96ec777eab3d67ff1b0bd350167e09ccd
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-07-14 13:38:34 -0400

    setup variables for group 2
    setup given variables and name and password parts of problem

--- Conditionals_Worksheet/ConditionalsWorksheet.js
+++ Conditionals_Worksheet/ConditionalsWorksheet.js
@@ -11,12 +11,33 @@ Conditionals Worksheet
 //alert("Testing");
 
 
-//Group 1 questions
+//Group 1 problem
 //Setup given variables
 var CarMPG = 15;
 var GasTankCapacity = 22;
-var GaugeReading = 50;
-var GaugeReadingPercent = GaugeReading / 100;
-var RemainingGas = GaugeReading * GasTankCapacity;
+var GaugeReading = 65;
+var GaugeReadingDecimal = GaugeReading / 100;
+var RemainingGas = GaugeReadingDecimal * GasTankCapacity;
+var DistanceToGo = 200;
 
 //Setup conditional with output
+if(GasTankCapacity * GaugeReadingDecimal * CarMPG >= DistanceToGo){
+    console.log("Yes you can make it without stopping for gas.")
+
+}else{console.log("You only have " + RemainingGas + " gallons left in your tank, better get gas while you can!")
+}
+
+//Group 2 problem
+//Setup given variables
+var UserName = "Spankiticus";
+var Password = 1234;
+var EnterUserName = prompt("Please enter your user name.");
+var EnterPassword = prompt("Please enter your password.");
+
+if(EnterUserName === UserName){
+
+}else console.log("User not found try again.");
+
+if(parseInt(EnterPassword) === Password){
+
+}else console.log("Password does not match our records");

08193a881dffe10027115b554b5579e6c3309c51
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-07-18 20:37:48 -0400

    end for loops
    end for loops

---
+++ Day6Math/js/Day6forLoops.js
@@ -0,0 +1,20 @@
+
+ /*
+Travis Johnson
+Section 00
+07/18/15
+Day 6 for loops
+ */
+
+
+//alert("Testing");
+
+ //basic for loop
+ //for(initialize counter); the conditions; increment of change)
+
+ for(var i=10; i>0; i--){
+     console.log("the value of i is " + i);
+ }
+
+ //Breaks - of this command is run, your loop will stop
+ for(var)

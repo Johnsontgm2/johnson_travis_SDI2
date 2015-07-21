e501a8e780469671cb45101fd92a69e6eb326ad1
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-07-21 18:18:13 -0400

    day 7 beers
    prompted user for beer number and validated

--- Day7/js/Day7Beer.js
+++ Day7/js/Day7Beer.js
@@ -9,9 +9,15 @@ Day 7 Beer
 //alert("Testing");
 
  //Create a loop that will write out the lyrics for the 99 bottles of beer song
+var numBeers = prompt("How many beers are on your wall");
+ while(numBeers === "" || isNaN(numBeers)){
+     numBeers = prompt("Please do not leave blank and only enter a number. \n How many beers are on your wall?");
 
+ }
+ //cast variable retrieved in prompt as number
+ numBeers=Number(numBeers);
  //for(initialize; condition; increment of change){}
- for(var i = 99; i > 0; i--){
+ for(var i = numBeers; i > 0; i--){
      //verify that the loop works
      //console.log(i);
      if(i===2) {

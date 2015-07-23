d081d24160564d88fad6a5c43beac2aa0eaa81d3
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-07-23 19:00:49 -0400

    old school way
    super specific function will only calculate the are of the rectangle
    10x20

--- Day 8/js/argumentsParam.js
+++ Day 8/js/argumentsParam.js
@@ -8,3 +8,27 @@ Day 8 Arguments and Parameters
 
 
 //alert("Testing");
+
+ //arguments - go in the function call these are the specific values
+ //parameters - go in the function definition and they catch the arguments(generic containers)
+
+ //Create variables for width and height
+ var width=10;
+ var height=20;
+
+
+ //create a function for area of a retangle
+ function calcArea(){
+     //create variables
+     var width=10;
+     var height=20;
+
+     //calculate the area
+     var area = width*height;
+
+     //console log area
+     console.log("The area of a rectange with a width of " + width + " and a height of " + height + " is " + area);
+ }
+ //call the function
+ calcArea();
+ calcArea();

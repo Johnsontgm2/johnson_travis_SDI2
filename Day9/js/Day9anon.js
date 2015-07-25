d2d7e8a0e96ffd179f693f712d853ffe9f329fa4
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-07-25 19:24:59 -0400

    day 9 anon
    day 9 anon

--- Day9/js/Day9anon.js
+++ Day9/js/Day9anon.js
@@ -7,4 +7,23 @@ Day 9 anon
  */
 
 
-alert("Testing");
+//alert("Testing");
+
+ //Basic Anonymous function
+ //var functionName = functions(){};
+
+ //Extremely popular in javascript and jquery
+ //Important in Object oriented programming
+ //For now, it is a matter of style choice between anon and regular
+
+ //create anon function to calculate the area of a triange
+ var triArea=function(b,h){
+     var area=.5*b*h;
+     return area;
+
+ };
+
+ //Function call for anon function
+ //MUST BE AFTER THE DEFINITION FOR ANON FUNCTION!!!!!!!!!*********!!!!!!
+ var results=triArea(10,20);
+ console.log(results);

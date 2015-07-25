3ee4904dc85d9e9c7f75a04b831697fec3fac9bb
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-07-25 17:38:15 -0400

    prompted and number casted min
    prompted and number casted

--- Day9/js/Day9random.js
+++ Day9/js/Day9random.js
@@ -8,3 +8,20 @@ Day 9 random number generator
 
 
 //alert("Testing");
+
+
+ //create a function that will take in user prompt max and min and return a random number between them
+
+ //Create a variable for the min value
+ var min =prompt("We are going to find a random number between two values. \nWhat is the minimum value?");
+ while(min==="" || isNaN(min)){
+     if(isNaN(min)){
+         min=prompt("Please only enter a number. What is the minimum value?");
+
+     }else{
+         min=prompt("Please do not leave blank what is the min value?");
+     }
+ }
+
+ //number cast or parseint
+ min=Number(min);
